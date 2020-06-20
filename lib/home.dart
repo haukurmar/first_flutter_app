509e17bd7cec13ce9f61e50f519e7c70e4cfda17
Haukur Bodvarsson <haukurmar@gmail.com>
2020-06-20 15:22:04 +0200

    move HomePage to a separate file.

---
+++ lib/home.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          widget.title,
+          style: TextStyle(
+              fontFamily: Theme.of(context).textTheme.headline6.fontFamily,
+              fontSize: Theme.of(context).textTheme.headline6.fontSize,
+              fontWeight: Theme.of(context).textTheme.headline6.fontWeight,
+              letterSpacing:
+                  Theme.of(context).textTheme.headline6.letterSpacing,
+              color: Colors.white),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+import 'home.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -42,63 +44,7 @@ class MyApp extends StatelessWidget {
             overline: GoogleFonts.openSans(
                 fontSize: 10, fontWeight: FontWeight.w400, letterSpacing: 1.5),
           )),
-      home: MyHomePage(title: 'My first Flutter app'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          widget.title,
-          style: TextStyle(
-              fontFamily: Theme.of(context).textTheme.headline6.fontFamily,
-              fontSize: Theme.of(context).textTheme.headline6.fontSize,
-              fontWeight: Theme.of(context).textTheme.headline6.fontWeight,
-              letterSpacing:
-                  Theme.of(context).textTheme.headline6.letterSpacing,
-              color: Colors.white),
-        ),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
+      home: HomePage(title: 'My first Flutter app'),
     );
   }
 }
